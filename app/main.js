e8d3ccda4f5b6c60056719865d7f322927bb893f
Mykolai_Lytvyn <mycdpepam@gmail.com>
2016-03-03 18:21:06 +0200

    Direcite in directive

--- app/main.js
+++ app/main.js
@@ -1,16 +1,48 @@
 var myApp = angular.module("myApp", []);
 
-myApp.controller("FirstCtrl", function ($scope) {
-   $scope.start = "start";
-   $scope.myReverse = function (){
-      return $scope.start.split("").reverse().join("");
-   }
+
+myApp.directive("parent", function () {
+    return {
+        restrict: "E",
+        templateUrl: "templates/parentTmpl.html",
+        transclude: true,
+
+        link: function () {
+            console.log("first");
+        },
+        controller: function ($scope) {
+            $scope.data = 0;
+
+            this.getData = function () {
+                return $scope.data
+            };
+
+            this.addData = function () {
+                $scope.data++;
+            };
+
+            this.removeData = function () {
+                $scope.data--;
+            };
+        }
+    }
 });
 
-myApp.directive("first", function () {
-   return {
-      restrict:"AECM",
-      templateUrl:"templates/firstTmpl.html",
-      controller: "FirstCtrl"
-   }
+myApp.directive("child", function () {
+    return {
+        restrict: "E",
+        require: "^parent",
+        templateUrl: "templates/childTmpl.html",
+        link: function (scope, element, attrs, firstCtrl) {
+            scope.parentData = firstCtrl.getData();
+
+            scope.addParentData = function () {
+                firstCtrl.addData();
+            };
+
+            scope.removeParentData = function () {
+                firstCtrl.removeData()
+            };
+        }
+    }
 });
