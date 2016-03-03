46792673cf3075b220f26e9b82b1d29e69f75e61
Mykolai_Lytvyn <mycdpepam@gmail.com>
2016-03-03 14:58:14 +0200

    Start

--- app/main.js
+++ app/main.js
@@ -1,8 +1,16 @@
 var myApp = angular.module("myApp", []);
 
 myApp.controller("FirstCtrl", function ($scope) {
-   $scope.start = "start"
+   $scope.start = "start";
    $scope.myReverse = function (){
       return $scope.start.split("").reverse().join("");
+   }
+});
+
+myApp.directive("first", function () {
+   return {
+      restrict:"AECM",
+      templateUrl:"templates/firstTmpl.html",
+      controller: "FirstCtrl"
    }
 });
