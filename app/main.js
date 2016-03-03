c9614660da65c533c55ead04edab2ccc97de28ab
Mykolai_Lytvyn <mycdpepam@gmail.com>
2016-03-03 13:32:20 +0200

    Filter and ng-filter

--- app/main.js
+++ app/main.js
@@ -1,8 +1,30 @@
-var myApp = angular.module("myApp", []);
+var myApp = angular.module("myApp", []).
+factory("Mans", function () {
+    return [
+        {
+            name: "Ivan",
+            age: 50
+        },
+        {
+            name: "Petr",
+            age: 100
+        },
+        {
+            name: "Vasiliy",
+            age: 15
+        }
+    ];
+}).
+filter("UpperCase", function () {
+    return function (data) {
+        return data.toUpperCase();
+    }
+});
 
-myApp.controller("FirstCtrl", function ($scope) {
-   $scope.start = "start"
-   $scope.myReverse = function (){
-      return $scope.start.split("").reverse().join("");
-   }
+
+myApp.controller("FirstCtrl", function ($scope, Mans) {
+    $scope.people = Mans;
+    $scope.myReverse = function () {
+        return $scope.start.split("").reverse().join("");
+    }
 });
