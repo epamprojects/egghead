f4e730568796f388cc7f495589c50608f60e9571
Mykolai_Lytvyn <mycdpepam@gmail.com>
2016-03-11 12:35:46 +0200

    angular.element

--- app/main.js
+++ app/main.js
@@ -1,16 +1,49 @@
-var myApp = angular.module("myApp", []);
+var myApp = angular.module("myApp", [])
 
-myApp.controller("FirstCtrl", function ($scope) {
-   $scope.start = "start";
-   $scope.myReverse = function (){
-      return $scope.start.split("").reverse().join("");
-   }
-});
+    .directive("dumpPassword1", function () {
+        return {
+            templateUrl: "templates/firstTmpl.html",
+            // work with element custom tag or it nested data
+            // if false  -> element this is "dump-password"
+            // if true  -> element this is "div.container"
+            replace: true,
+            scope:{},
+            link: function (scope, element) {
+                scope.$watch("model.input", function (value) {
+                    if (value === "password") {
+                        // elem - this is jqueryLite (easy jQuery)
+                        element.find("input").next().addClass("alert alert-success")
+                    } else {
+                        element.find("input").next().removeClass("alert alert-success")
+                    }
+                })
+            }
+        }
+    })
 
-myApp.directive("first", function () {
-   return {
-      restrict:"AECM",
-      templateUrl:"templates/firstTmpl.html",
-      controller: "FirstCtrl"
-   }
-});
+    .directive("dumpPassword2", function () {
+        var validElement =  angular.element("<div class='valid'>{{model.input}}</div>");
+        return {
+            templateUrl: "templates/secondTmpl.html",
+            replace: true,
+
+            // if using this function it is possible to add
+            // addition DOM to directive
+            // as result link will be using as result of return compile anonymous function
+            compile: function (tElem) {
+                tElem.append(validElement);
+
+                return function (scope, element) {
+                    scope.$watch("model.input", function (value) {
+                        if (value === "password") {
+                            // angular allow work with adding element in in a manner
+                            validElement.addClass("alert alert-success")
+                        } else {
+                            validElement.removeClass("alert alert-success")
+                        }
+                    })
+                }
+            }
+        }
+    });
+
